f4a37f6997310e0a656ca03bc50decc0d699b66f
neal.ho <neal.ho@gigabyte.com>
2022-06-27 13:58:35 +0800

    init

---
+++ GDesign2022NFT.Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalkingTec.Mvvm.Core;
+
+namespace GDesign2022NFT.Model
+{
+    public class User:TopBasePoco
+    {
+    }
+}
